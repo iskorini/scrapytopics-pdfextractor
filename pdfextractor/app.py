a1a2832e16667d03bcbb88b265397c10b77454c1
Federico Schipani <federicoschipani@MacBook-Pro-di-Federico.local>
2025-04-29 16:04:06 +0200

    fix: update pdf_handler to handle base64 input and cache text extraction

--- pdfextractor/app.py
+++ pdfextractor/app.py
@@ -3,6 +3,9 @@ from pypdf import PdfReader
 import boto3
 import io
 import logging
+import base64
+import hashlib
+import os
 
 logger = logging.getLogger()
 logger.setLevel("INFO")
@@ -11,23 +14,33 @@ app = Chalice(app_name='PDFExtractor')
 
 s3 = boto3.client(service_name="s3")
 
-@app.lambda_function()
+@app.route("/extract", methods=["POST"], content_types=['application/json'], cors=True)
 def pdf_handler(event, context):
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = event['Records'][0]['s3']['object']['key']
+    request = app.current_request
+    body = request.json_body
     logger.info(event)
     try:
-        pdf = s3.get_object(Bucket=bucket, Key=key)
-        body = pdf["Body"].read()
-        reader = PdfReader(io.BytesIO(body))
+        file_b64 = body["file_content"]
+        file_bytes = base64.b64decode(file_b64)
+        file_hash = hashlib.md5(file_b64).hexdigest()
+        key = f"txt/{file_hash}.txt"
+        ### check in cache
+        try:
+            text = s3.get_object(Bucket=os.getenv("BUCKET"), Key=key)
+            text = text["Body"].read().decode("utf-8")
+            return {"filename": body["filename"], "text": text, "cached": True}
+        except s3.exception.NoSuchKey:
+            pass 
+        ###
+        reader = PdfReader(io.BytesIO(file_bytes))
         text = ""
         logger.info(f"PDF Pages {str(len(reader.pages))}")
         for page in reader.pages:
             text += page.extract_text()
         logger.info("text extracted")
-        output_key = f"txt/{key.rsplit('.', 1)[0]}.txt"
-        s3.put_object(Bucket=bucket, Key=output_key, Body=text)
-        logger.info(f"text saved in {bucket}/{output_key}")
+        s3.put_object(Bucket = os.getenv("BUCKET"), Key = key, body=text.encode("utf-8"))
+        logger.info(f"text saved in {os.getenv("BUCKET")}/{key}")
+        return {"filename": body["filename"], "text": text, "cached": False}
     except Exception as e:
         print(e)
         raise e
--- terraform/main.tf
+++ terraform/main.tf
@@ -85,24 +85,3 @@ resource "aws_iam_role_policy_attachment" "lambda_s3_attach" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = aws_iam_policy.lambda_s3_policy.arn
 }
-
-resource "aws_lambda_permission" "allow_s3" {
-  statement_id  = "AllowS3Invoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.pdfextractor_lambda.arn
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.scrapy_topics_bucket.arn
-}
-
-resource "aws_s3_bucket_notification" "s3_to_lambda" {
-  bucket = aws_s3_bucket.scrapy_topics_bucket.id
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.pdfextractor_lambda.arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "pdf/"
-    filter_suffix       = ".pdf"
-  }
-
-  depends_on = [aws_lambda_permission.allow_s3]
-}
