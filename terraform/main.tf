45e941ae91cacc0ed9559998579986556b28fb41
Federico Schipani <federicoschipani@MacBook-Pro-di-Federico.local>
2025-04-29 16:27:24 +0200

    fix: remove production stage and output URL from API Gateway deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -129,9 +129,4 @@ resource "aws_lambda_permission" "allow_api_gateway" {
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [aws_api_gateway_integration.lambda]
   rest_api_id = aws_api_gateway_rest_api.extract-pdf-api.id
-  stage_name  = "prod"
-}
-
-output "api_url" {
-  value = "${aws_api_gateway_deployment.deployment.invoke_url}/extract"
 }
