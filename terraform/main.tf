a1a6eade758495c7f47ead15fefa823284f47ff9
Federico Schipani <federicoschipani@MacBook-Pro-di-Federico.local>
2025-04-29 15:17:36 +0200

    fix: update Lambda deployment workflow to include layer handling and refactor IAM roles

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,34 +16,45 @@ resource "aws_s3_bucket" "scrapy_topics_bucket" {
     force_destroy = true
 }
 
+resource "aws_lambda_layer_version" "pdfextractor_layer" {
+  filename         = "${path.module}/layer.zip"
+  layer_name       = "pdfextractor-layer"
+  compatible_runtimes = ["python3.12"]
+  source_code_hash = filebase64sha256("${path.module}/layer.zip")
+}
+
 resource "aws_lambda_function" "pdfextractor_lambda" {
-    function_name = var.lambda_function_name
-    filename      = "deployment.zip"
-    handler       = "app.pdf_handler"
-    runtime       = "python3.12"
-    role          = aws_iam_role.lambda_role.arn
+  function_name = "pdfextractor"
+  filename      = "${path.module}/deployment.zip"
+  source_code_hash = filebase64sha256("${path.module}/deployment.zip")
+  handler       = "app.pdf_handler"
+  runtime       = "python3.12"
+  role          = aws_iam_role.lambda_exec.arn
+  layers        = [aws_lambda_layer_version.pdfextractor_layer.arn]
 }
 
-resource "aws_iam_role" "lambda_role" { 
-    name = "${var.lambda_function_name}_lambda_role"
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Action = "sts:AssumeRole"
-                Effect = "Allow"
-                Principal = {
-                    Service = "lambda.amazonaws.com"
-                }
-            }
-        ]
-    })
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid    = ""
+    }]
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
-  role       = aws_iam_role.lambda_role.name
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
 
 resource "aws_iam_policy" "lambda_s3_policy" {
   name = "${var.lambda_function_name}_s3_policy"
