18d7906715158346869db7f6eef9e7299481e4a5
Federico Schipani <federicoschipani@MacBook-Pro-di-Federico.local>
2025-05-01 10:38:13 +0200

    fix: add default stage name to API Gateway deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -129,4 +129,5 @@ resource "aws_lambda_permission" "allow_api_gateway" {
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [aws_api_gateway_integration.lambda]
   rest_api_id = aws_api_gateway_rest_api.pdf-api.id
+  stage_name = "default"
 }
