8e853603faafa37b1bb66fb61157eac4aabee868
Federico Schipani <federicoschipani@MacBook-Pro-di-Federico.local>
2025-05-03 16:55:13 +0200

    fix: update API Gateway integration response for OPTIONS method to include CORS headers

--- terraform/main.tf
+++ terraform/main.tf
@@ -158,21 +158,29 @@ resource "aws_api_gateway_integration" "options" {
   request_templates = {
     "application/json" = "{\"statusCode\": 200}"
   }
+}
 
-  integration_response {
-    status_code = "200"
+resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
+  rest_api_id = aws_api_gateway_rest_api.pdf-api.id
+  resource_id = aws_api_gateway_resource.extract.id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = "200"
 
-    response_parameters = {
+  response_parameters = {
       "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
       "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
       "method.response.header.Access-Control-Allow-Origin"  = "'*'" # TODO: Change to your domain
     }
 
-    response_templates = {
-      "application/json" = ""
-    }
+  response_templates = {
+    "application/json" = ""
+  }
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
   }
 }
+}
+
 
 resource "aws_api_gateway_method_response" "options" {
   rest_api_id = aws_api_gateway_rest_api.pdf-api.id
