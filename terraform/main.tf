e781893f0add97fc1c1040ec025e5308e14ca303
Federico Schipani <federicoschipani@MacBook-Pro-di-Federico.local>
2025-05-03 16:48:38 +0200

    feat: add OPTIONS method and CORS support for API Gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -141,3 +141,52 @@ resource "aws_api_gateway_stage" "default" {
   rest_api_id   = aws_api_gateway_rest_api.pdf-api.id
   deployment_id = aws_api_gateway_deployment.deployment.id
 }
+
+resource "aws_api_gateway_method" "options" {
+  rest_api_id = aws_api_gateway_rest_api.pdf-api.id
+  resource_id = aws_api_gateway_resource.extract.id
+  http_method = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "options" {
+  rest_api_id             = aws_api_gateway_rest_api.pdf-api.id
+  resource_id             = aws_api_gateway_resource.extract.id
+  http_method             = aws_api_gateway_method.options.http_method
+  type                    = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+
+  integration_response {
+    status_code = "200"
+
+    response_parameters = {
+      "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
+      "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
+      "method.response.header.Access-Control-Allow-Origin"  = "'*'" # TODO: Change to your domain
+    }
+
+    response_templates = {
+      "application/json" = ""
+    }
+  }
+}
+
+resource "aws_api_gateway_method_response" "options" {
+  rest_api_id = aws_api_gateway_rest_api.pdf-api.id
+  resource_id = aws_api_gateway_resource.extract.id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
